feffa668ca8a39f12057a647688142aeb7a45776
Jessica <jessikareix20@gmail.com>
2023-07-04 18:46:36 +0200

    FEAT: Added changed icon for buttons also an border for footer and picture

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,19 +6,22 @@ const projectsCollection = defineCollection({
 	  title: z.string(),
 	  description: z.string(),
 	  tags: z.array(z.string()),
-	  web_url: z.string().optional(),
 	  links: z.array(
 		z.object({
 		  url: z.string(),
 		  label: z.string(),
 		})
 	  ),
-	  img: z.string().optional(),
 	  img_alt: z.string().optional(),
+	  img: z.string().optional(),
 	}),
+	
   });
+
+
   
 
 export const collections = {
 	projects: projectsCollection,
-};
+}
+	
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client-image" />
+/// <reference types="astro/client-image" />
